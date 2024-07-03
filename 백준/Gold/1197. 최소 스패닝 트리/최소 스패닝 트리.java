2fc53f01bb89981783eeb6cd45564f5051827f99
Shanate <109050481+Shanate@users.noreply.github.com>
2024-07-03 11:37:36 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 824 ms, Memory: 52824 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,49 +1,56 @@
-
+//Using Prim Algorithm
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
-	static int V;
-	static int[][] g;
-	static int[] p;
+	static int N,M;
+	static int A,B,C; // A, B : 정점, C : 가중치
+	static List<int[]>[] g;
+	static boolean[] v;
+	static int[] w;
 	
-	static void make() {
-		p = new int[V];
-		for (int i=0; i<V; i++) p[i] = i;
-	}
-	static int find(int a) {
-		if (p[a] == a) return a;
-		return p[a] = find(p[a]);
-	}
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if (aRoot == bRoot) return false;
-		p[bRoot] = aRoot;
-		return true;
-	}
-	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(System.in);
-		 V = sc.nextInt();
-		 int E = sc.nextInt();
-		 g=new int[E][3];
-		for(int i=0; i<E; i++) {
-			int A = sc.nextInt();//from
-			int B = sc.nextInt();//to
-			int C = sc.nextInt();//cost
-			g[i] = new int[] {A,B,C};
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		g = new List[N];
+		for(int i=0; i<N; i++) {
+			g[i] = new ArrayList<>();
 		}
-		Arrays.sort(g,(o1,o2)->Integer.compare(o1[2], o2[2]));
-		make();
-		int sum=0, cnt=0;
-		for(int[] e:g) {
-			if(union(e[0]-1,e[1]-1)) {
-				sum += e[2];
-				if(++cnt==V-1) break;
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			A = Integer.parseInt(st.nextToken())-1;
+			B = Integer.parseInt(st.nextToken())-1;
+			C = Integer.parseInt(st.nextToken());
+			g[A].add(new int[] {B,C});
+			g[B].add(new int[] {A,C});
+		}
+			v = new boolean[N];
+			w = new int[N];
+			for(int i=0; i<N; i++) w[i]=Integer.MAX_VALUE;
+			int sum=0, cnt=0;
+			w[0]=0; 
+			for(int i=0; i<N; i++) {
+				int min = Integer.MAX_VALUE;
+				int minVertex = -1;
+				for(int j=0; j<N; j++) { 
+					if(!v[j] && min>w[j]) {
+								min=w[j];
+								minVertex=j;
+					}
+				}
+				v[minVertex]=true;
+				sum += min;
+				if(cnt++==N-1) break;
+				for(int[] j:g[minVertex]) {
+					if(!v[j[0]] && w[j[0]]>j[1]) {
+								   w[j[0]]=j[1];//
+					}
+				}
 			}
-		}
-		System.out.println(sum);
-		sc.close();
+			System.out.println(sum);
 	}
 }
