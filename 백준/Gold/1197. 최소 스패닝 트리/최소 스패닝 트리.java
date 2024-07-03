74da76fb7a3c937622e6b11b14f35b9af034cdbd
Shanate <109050481+Shanate@users.noreply.github.com>
2024-07-03 15:44:36 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 568 ms, Memory: 53476 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,25 +1,22 @@
-//Using Prim Algorithm
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
+public class Main{
 	static int N,M;
-	static int A,B,C; // A, B : 정점, C : 가중치
+	static int A,B,C;
 	static List<int[]>[] g;
 	static boolean[] v;
 	static int[] w;
-	
-	public static void main(String[] args) throws Exception {
 
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		
 		g = new List[N];
-		for(int i=0; i<N; i++) {
-			g[i] = new ArrayList<>();
-		}
+		for(int i=0; i<N; i++) g[i] = new ArrayList<>();
+		
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine()," ");
 			A = Integer.parseInt(st.nextToken())-1;
@@ -28,29 +25,35 @@ public class Main {
 			g[A].add(new int[] {B,C});
 			g[B].add(new int[] {A,C});
 		}
-			v = new boolean[N];
-			w = new int[N];
-			for(int i=0; i<N; i++) w[i]=Integer.MAX_VALUE;
-			int sum=0, cnt=0;
-			w[0]=0; 
-			for(int i=0; i<N; i++) {
-				int min = Integer.MAX_VALUE;
-				int minVertex = -1;
-				for(int j=0; j<N; j++) { 
-					if(!v[j] && min>w[j]) {
-								min=w[j];
-								minVertex=j;
-					}
-				}
-				v[minVertex]=true;
-				sum += min;
-				if(cnt++==N-1) break;
-				for(int[] j:g[minVertex]) {
-					if(!v[j[0]] && w[j[0]]>j[1]) {
-								   w[j[0]]=j[1];//
-					}
+		
+		v =new boolean[N];
+		w = new int[N];
+		for(int i=0; i<N; i++) w[i] = Integer.MAX_VALUE;
+		
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)->Integer.compare(o1[1], o2[1]));
+		int sum=0, cnt=0;
+		w[0]=0;
+		pq.offer(new int[] {0,0});
+		while(!pq.isEmpty()) {
+			int[] vc = pq.poll();
+			int min = vc[1];
+			int minVertex = vc[0];
+			if(v[minVertex]) continue;
+			
+			v[minVertex]=true;
+			sum += min;
+			if(cnt++==N-1) break;
+			
+			for(int[] j:g[minVertex]) {
+				if(!v[j[0]] && w[j[0]]>j[1]) {
+					w[j[0]]=j[1];
+					pq.offer(j);
 				}
 			}
-			System.out.println(sum);
+		}
+		                       System.out.println(sum);  
 	}
+	
+	
+	
 }
