f9713545167e183290c36d8754dc58f1bc78f5cf
Shanate <109050481+Shanate@users.noreply.github.com>
2024-07-24 22:56:42 +0900

    [Silver IV] Title: Brackets, Time: 124 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Silver/7585. Brackets/Brackets.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static String ans(String str) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if(c=='(' || c=='[' || c=='{'){ // 개괄호일 경우 stack에 삽입
+                stack.push(c);
+            }
+            else if(c == ')'){
+                if(stack.empty() || stack.peek() != '('){
+                    return "Illegal";
+                }
+                else{
+                    stack.pop();
+                }
+            }
+            else if(c == ']'){
+                if(stack.empty() || stack.peek() != '['){
+                    return "Illegal";
+                }
+                else{
+                    stack.pop();
+                }
+            }
+            else if(c == '}'){
+                if(stack.empty() || stack.peek() != '{'){
+                    return "Illegal";
+                }
+                else{
+                    stack.pop();
+                }
+            }
+        }
+        if(stack.empty()) {
+            return "Legal";
+        }
+        else{
+            return "Illegal";
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        StringBuffer sb = new StringBuffer();
+        String str;
+        while(true){
+            str = br.readLine();
+            if(str.equals("#")){
+                break;
+            }
+            sb.append(ans(str)).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
