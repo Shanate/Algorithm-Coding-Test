db45d29092ae9c8b4317cc0de4c2e3115882bfb1
Shanate <109050481+Shanate@users.noreply.github.com>
2024-07-28 11:06:37 +0900

    [Bronze II] Title: OX퀴즈, Time: 144 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            String s = br.readLine();
+            int add = 0;
+            int score = 0;
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == 'O') {
+                    add++;
+                    score += add;
+                } else {
+                    add = 0;
+                }
+            }
+            sb.append(score + "\n");
+        }
+        System.out.print(sb);
+    }
+}
