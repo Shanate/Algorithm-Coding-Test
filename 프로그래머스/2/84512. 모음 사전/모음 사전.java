24fbc59d488b59fa9f85f182349aa987c561ab78
Shanate <109050481+Shanate@users.noreply.github.com>
2024-08-06 17:26:39 +0900

    [level 2] Title: 모음 사전, Time: 34.70 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+/*
+1st thinking : word의 길이는 1이상 5이하이다. => 각 자리에 들어올 수 있는 경우의 수는 5가지이다.
+5 X 5 X 5 X 5 X 5 = 3,125가 이 사전의 총 단어 개수
+A -> E가 아니라, A -> AA다.
+A -> AA -> AAA -> AAAA -> AAAAA -> AAAAE -> AAAAI -> AAAAO -> AAAAU ->
+AAAE -> AAAI -> AAAO -> AAAU -> AAE ...
+? 멀리 갔다가 다시 돌아오는데
+재귀인가? -> DFS?
+를 고려해봐야할거같다.
+*/
+
+class Solution {
+        static char[] str = {'A','E','I','O','U'}; // 모음
+        static ArrayList<String> dic = new ArrayList<>();
+
+    private static void dfs(String word) {
+        if(word.length()> 5) return; // 종료조건.
+        dic.add(word);
+        for(char vowel : str) {
+            dfs(word + vowel);
+        }
+    }
+
+    static int solution(String word){
+        int answer = 0;
+        dic.clear();
+        dfs("");
+
+
+        return dic.indexOf(word);
+    }
+}
