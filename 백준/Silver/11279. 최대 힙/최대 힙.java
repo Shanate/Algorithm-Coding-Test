abc7c77e431b4abb33c144599c67a723bcef2949
Shanate <109050481+Shanate@users.noreply.github.com>
2024-08-12 10:28:03 +0900

    [Silver II] Title: 최대 힙, Time: 796 ms, Memory: 35704 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            pq.offer(num);
+            if(num == 0){
+                if(pq.isEmpty()){
+                    System.out.println(0);
+                    pq.poll();
+                }
+                else {
+                    System.out.println(pq.poll());
+                }
+            }
+        }
+    }
+}
