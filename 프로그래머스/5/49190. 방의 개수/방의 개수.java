50026d970f4c3da91dfd40bc284020bfe1d0e32e
Shanate <109050481+Shanate@users.noreply.github.com>
2024-09-27 14:29:00 +0900

    [level 5] Title: 방의 개수, Time: 200.64 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.java
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.java
@@ -1,61 +1,76 @@
 import java.util.*;
 class Solution {
-
-    static Map<Node, HashSet<Node>> map = new HashMap<>();
-
-    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    static int[] dy = {0, 1, 1, 1, 0, -1 ,-1 ,-1};
-
-    static int solution(int[] arrows) {
+    public int solution(int[] arrows) {
         int answer = 0;
-
-        map.put(new Node(0, 0), new HashSet<>());
-
-        Node cur = new Node(0, 0);
-
-        for(int i = 0 ; i < arrows.length; i++) {
-
-            int direction = arrows[i];
-
-            for (int j = 0; j < 2; j++) {
-                int x = cur.x + dx[direction];
-                int y = cur.y + dy[direction];
-
-                Node next = new Node (x, y); //다음으로 방문할 점
-
-                if (!map.containsKey(next)) {
-                    //다음으로 방문할 점이, 아직 방문하지 않은 점이라면
-                    map.put(next, new HashSet<>()); //key에 추가함으로서 방문 표시
-                } else if (map.containsKey(next) && !map.get(cur).contains(next)) {
-                    //다음으로 방문할 점이, 이미 방문했지만 현재 점에서는 방문하지 않았으면
-                    answer++; //방이 만들어진다.
-                }
-
-                map.get(cur).add(next); //현재 점에서 다음으로 방문할 점 방문 표시
-                map.get(next).add(cur); //
-
-                cur = next;
+        int minW = 0, maxW = 0, minH = 0, maxH = 0;
+        int width = 0, height = 0;
+        
+        for(int i = 0; i < arrows.length; i++) {
+            if (arrows[i] == 0 || arrows[i] == 1 || arrows[i] == 7)
+                height++;
+            maxH = Math.max(maxH, height);
+            if (arrows[i] == 3 || arrows[i] == 4 || arrows[i] == 5)
+                height--;
+            minH = Math.min(minH, height);
+            if (arrows[i] == 1 || arrows[i] == 2 || arrows[i] == 3)
+                width++;
+            maxW = Math.max(maxW, width);
+            if (arrows[i] == 5 || arrows[i] == 6 || arrows[i] == 7)
+                width--;
+            minW = Math.min(minW, width);
+        }
+        int x = maxH, y = -minW; //원점의 위치
+        width = maxW - minW + 1;
+        height = maxH - minH + 1;
+        
+        //좌표 (a,b)가 어떤 좌표와 연결되어있는지
+        HashMap<Integer, ArrayList<Integer>> connect = new HashMap<>();
+        connect.put(x*width+y, new ArrayList<>());
+        
+        boolean[][] visited = new boolean[height][width];
+        visited[x][y] = true; //시작점은 이미 지나간거니까
+        for(int i = 0; i < arrows.length; i++) {
+            int prev_x = x, prev_y = y;
+            
+            if (arrows[i] == 0 || arrows[i] == 1 || arrows[i] == 7)
+                x -= 1;
+            if (arrows[i] == 3 || arrows[i] == 4 || arrows[i] == 5)
+                x += 1;
+            if (arrows[i] == 1 || arrows[i] == 2 || arrows[i] == 3)
+                y += 1;
+            if (arrows[i] == 5 || arrows[i] == 6 || arrows[i] == 7)
+                y -= 1;
+            // //방문하지 않은 점이면 Map에도 해당 점 원소 추가
+            if (!connect.containsKey(x*width+y)) {
+                connect.put(x*width+y, new ArrayList<>());
             }
+            //해당 점을 한번 더 방문하고, 이전에 그어지지 않았던 선이면
+            if (visited[x][y] == true) { 
+                if (connect.get(x*width+y).contains(prev_x*width+prev_y) == false)
+                    answer++;
+            }
+            //대각선 검사 : 교차할때 방 만들어짐, 단 이미 그었던 선은 안됨.
+            if (connect.get(x*width+y).contains(prev_x*width+prev_y) == false) {
+                if (arrows[i] == 7 && connect.containsKey((x+1)*width+y)) 
+                    if (connect.get((x+1)*width+y).contains(x*width+(y+1)) == true) 
+                            answer++;
+                if (arrows[i] == 5 && connect.containsKey((x-1)*width+y)) 
+                    if (connect.get((x-1)*width+y).contains(x*width+(y+1)) == true)
+                            answer++;
+                if (arrows[i] == 3 && connect.containsKey((x-1)*width+y)) 
+                    if (connect.get((x-1)*width+y).contains(x*width+(y-1)) == true)
+                            answer++;
+                if (arrows[i] == 1 && connect.containsKey(x*width+(y-1)))
+                    if (connect.get(x*width+(y-1)).contains((x+1)*width+y) == true)
+                            answer++;
+            }
+            if (visited[x][y] == false) {
+                visited[x][y] = true;
+            }
+            // 점 연결
+            connect.get(x*width+y).add(prev_x*width+prev_y);
+            connect.get(prev_x*width+prev_y).add(x*width+y);
         }
         return answer;
-    }
-    static class Node {
-        public int x;
-        public int y;
-
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override //HashSet과 HashMap에 저장하기 위해
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-
-        @Override //각 점을 비교하여 같은 점인지 확인하기 위해
-        public boolean equals(Object o) {
-            return this.x == ((Node) o).x && this.y == ((Node) o).y;
-        }
     }
 }
