658f0bcfcf2f3f65aea42225988c4f37db41c58f
Shanate <109050481+Shanate@users.noreply.github.com>
2024-10-03 19:03:08 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 16988 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+
+        perm(1, 0);
+        System.out.println(sb);
+    }
+
+    static void perm(int start, int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            arr[depth] = i;
+            perm(i, depth + 1);
+        }
+
+
+    }
+}
