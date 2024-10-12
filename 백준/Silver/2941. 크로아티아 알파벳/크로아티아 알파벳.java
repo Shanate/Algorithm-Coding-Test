bb9fabc3ab4000d28b18d54e61397704b8a9e955
Shanate <109050481+Shanate@users.noreply.github.com>
2024-10-12 13:30:55 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -5,14 +5,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        str = str.replaceAll("c=", ".");
-        str = str.replaceAll("c-", ".");
-        str = str.replaceAll("dz=", ".");
-        str = str.replaceAll("d-", ".");
-        str = str.replaceAll("lj", ".");
-        str = str.replaceAll("nj", ".");
-        str = str.replaceAll("s=", ".");
-        str = str.replaceAll("z=", ".");
+        str = str.replaceAll("c=", ".").replaceAll("c-", ".")
+                .replaceAll("dz=", ".").replaceAll("d-", ".")
+                .replaceAll("lj", ".").replaceAll("nj", ".")
+                .replaceAll("s=", ".").replaceAll("z=",".");
 
         System.out.println(str.length());
     }
