8a773ad5b8dde14e2a2bbd44655b96a67456e970
Shanate <109050481+Shanate@users.noreply.github.com>
2024-11-08 13:15:22 +0900

    [Silver II] Title: DFS와 BFS, Time: 308 ms, Memory: 25240 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,58 +1,58 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-		static int N, M, V;
-		static int[][] g;
-		static boolean[] v;
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		V = Integer.parseInt(st.nextToken());
-		g = new int[N][N];
-		v = new boolean[N];
-		
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			int from = Integer.parseInt(st.nextToken())-1; //시정점
-			int   to = Integer.parseInt(st.nextToken())-1; //종정점
-			g[from][to]=1;
-			g[to][from]=1;
-		}
-//		System.out.println(Arrays.deepToString(g));
-		dfs(V-1); // 탐색 시작 정점부터 시작하기
-		System.out.println();
-		Arrays.fill(v, false);
-		bfs(V-1);
-	}
 
-	 static void bfs(int i) {
-		ArrayDeque<Integer> q=new ArrayDeque<>();
-		v[i]=true;
-		q.offer(i);
-		while(!q.isEmpty()) {
-			i=q.poll();
-			System.out.print((i+1)+" ");
-			for(int j=0; j<N; j++) {
-				if(g[i][j] != 0 && !v[j]) {
-					v[j]=true;
-					q.offer(j);
-				}
-			}
-		}
-	}
+    static int N, M, V;
+    static int[][] g;
+    static boolean[] v;
 
-	static void dfs(int i) {
-		v[i]=true;
-		System.out.print((i+1)+" ");
-		for(int j=0; j<N; j++) {
-			if(g[i][j] != 0 && !v[j]) {
-				dfs(j);
-			}
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken()); // 정점 개수
+        M = Integer.parseInt(st.nextToken()); // 간선 개수
+        V = Integer.parseInt(st.nextToken()); // 탐색 시작 정점
+        g = new int[N][N]; // 그래프 인접 행렬
+        v = new boolean[N]; // 방문 여부 배열
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken()) - 1; // 시정점
+            int to = Integer.parseInt(st.nextToken()) - 1; // 종정점
+            g[from][to] = 1; // 양방향 간선이기 때문에
+            g[to][from] = 1; // 동일하게 =1로 설정
+        }
+        dfs(V - 1); // 탐색 시작 정점부터 시작하기
+        System.out.println();
+        Arrays.fill(v, false);
+        bfs(V - 1);
+    }
+
+    static void bfs(int i) {
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        v[i] = true;
+        queue.offer(i);
+        while(!queue.isEmpty()) {
+            i=queue.poll();
+            System.out.print((i+1)+" ");
+            for(int j=0;j<N;j++) {
+                if(g[i][j] != 0 && !v[j]) {
+                    v[j] = true;
+                    queue.offer(j);
+                }
+            }
+        }
+    }
+
+    static void dfs(int i) {
+        v[i] = true;
+        System.out.print((i+1)+" ");
+        for(int j=0;j<N;j++) {
+            if(g[i][j] != 0 && !v[j]) {
+                dfs(j);
+            }
+        }
+    }
 }
