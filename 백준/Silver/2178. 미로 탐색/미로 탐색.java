d186723cc9a62d9b44f459d591612dd8cda54a2d
Shanate <109050481+Shanate@users.noreply.github.com>
2024-12-31 17:55:02 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,44 +3,48 @@ import java.util.*;
 
 public class Main {
 
-    static int N, M; // 맵 크기
+    static int N, M;
     static boolean[][] visited;
-    static int[] di = {-1, 0, 1, 0};    // 상, 우, 하, 좌 (4방 탐색)
-    static int[] dj = { 0, 1, 0, -1};
+    static int[] di = {-1, 0, 1, 0};
+    static int[] dj = {0, 1, 0, -1};
     static int[][] map;
-    static int[][] distance; // 각 위치까지의 최단 거리를 저장하는 2차원 배열
+    static int[][] distance;
 
     public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
         visited = new boolean[N][M];
-        distance = new int[N][M]; // 선택한 좌표까지 거리를 더하기 위한 매트릭스. == 카운팅
+        distance = new int[N][M];
 
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
+            String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = s.charAt(j) - '0';
+                map[i][j] = str.charAt(j) - '0';
             }
         }
-        bfs(0,0);
-        System.out.println(distance[N-1][M-1]);
 
-    } // Main
+        bfs(0, 0);
+        
+        System.out.println(distance[N - 1][M - 1]);
+
+    }
 
     static void bfs(int i, int j) {
-        ArrayDeque<int[]> q = new ArrayDeque<>(); // 'q' 어레이덱에 int[] 형식을 저장
-        q.add(new int[]{i, j}); // i행과 j열을 어레이덱에 추가 // 시작 좌표 (0,0)을 큐에 추가
-        visited[i][j] = true; // 시작 지점을 방문 처리
-        distance[i][j] = 1; // 시작 지점에서의 거리를 초기화 (시작점도 카운팅한다 하였으니 '1'로 결정)
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{i, j});
+        visited[i][j] = true;
+        distance[i][j] = 1;
 
-        while(!q.isEmpty()) {
-            int[] cur = q.poll(); // 큐에서 현재 위치를 꺼낸다.
-            int curX = cur[0]; // 현재 X좌표
-            int curY = cur[1]; // 현재 Y좌표
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int curX = cur[0];
+            int curY = cur[1];
 
             for (int k = 0; k < 4; k++) {
                 int nextX = curX + di[k]; // 다음 X좌표
@@ -48,7 +52,7 @@ public class Main {
 
                 // 미로 범위 내인가? 그렇다면 길이 있는가? 방문해본 적이 있는가?
                 if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < M && map[nextX][nextY] == 1 && !visited[nextX][nextY]) {
-                    q.offer(new int[]{nextX, nextY}); // 큐에 다음 위치를 추가
+                    queue.offer(new int[]{nextX, nextY}); // 큐에 다음 위치를 추가
                     visited[nextX][nextY] = true; // 방문 처리
                     distance[nextX][nextY] = distance[curX][curY] + 1; // 이전 위치 거리 + 1
 
