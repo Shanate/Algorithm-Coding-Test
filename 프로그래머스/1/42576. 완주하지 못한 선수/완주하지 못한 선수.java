efd0571749a8cd32c5a53c8c164443eff8764f96
Shanate <109050481+Shanate@users.noreply.github.com>
2025-01-08 20:33:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.56 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,17 +3,19 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        for (String participants : participant) { // 참가자를 카운팅. 동명이인 가능성을 두고 getOrDefault 사용
-            hashMap.put(participants, hashMap.getOrDefault(participants, 0) + 1);
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
-        for (String completions : completion) { // 참가자를 디카운팅. 0이 되면 레이스 완주 성공
-            hashMap.put(completions, hashMap.get(completions) - 1);
+        for(String name : completion) {
+            map.put(name, map.getOrDefault(name, 0) - 1);
         }
 
-        for(String key : hashMap.keySet()) { // 완주하지 못한 선수 색출
-            if (hashMap.get(key) != 0) {
-                answer = key;
+        for(String name : map.keySet()) {
+            if(map.get(name) != 0) {
+                answer = name;
             }
         }
 
