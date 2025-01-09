5c201550740013a7140c78b91e825c9c43f248c6
Shanate <109050481+Shanate@users.noreply.github.com>
2025-01-09 22:10:43 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,6 +1,7 @@
 import java.util.*;
+
 class Solution {
-    public int solution(String[][] clothes) {
+    static int solution(String[][] clothes) {
         int answer = 1;
         HashMap<String,Integer> map = new HashMap<>();
 
