537dbe39414211e0da74467327c322f4a7a27762
Shanate <109050481+Shanate@users.noreply.github.com>
2025-02-23 15:49:01 +0900

    [Bronze II] Title: 피자는 나눌 수록 커지잖아요, Time: 276 ms, Memory: 21512 KB -BaekjoonHub

---
+++ 백준/Bronze/17946. 피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // 테스트 케이스의 수
+        for (int i = 0; i < T; i++) {
+            int K = sc.nextInt(); // 최대 칼질 수 (사용하지 않아도 됨)
+            System.out.println(1);
+        }
+    }
+}
