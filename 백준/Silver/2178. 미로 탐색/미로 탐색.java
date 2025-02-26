68dc801182bbca1e291e8e95ba837d0b0cb91185
Shanate <109050481+Shanate@users.noreply.github.com>
2025-02-26 21:23:41 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,42 +4,34 @@ import java.util.*;
 public class Main {
 
     static int N, M;
-    static boolean[][] visited;
-    static int[] di = {-1, 0, 1, 0};
-    static int[] dj = {0, 1, 0, -1};
     static int[][] map;
-    static int[][] distance;
+    static boolean[][] visited;
+    static int[] di = {1, -1, 0, 0}; // 상하좌우
+    static int[] dj = {0, 0, 1, -1};
 
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         map = new int[N][M];
         visited = new boolean[N][M];
-        distance = new int[N][M];
-
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+                String s = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = str.charAt(j) - '0';
+                map[i][j] = s.charAt(j) - '0';
             }
         }
 
-        bfs(0, 0);
-        
-        System.out.println(distance[N - 1][M - 1]);
-
+        bfs(0, 0); // 시작점
+        System.out.println(map[N-1][M-1]);
+//        System.out.println(Arrays.deepToString(map));
     }
 
-    static void bfs(int i, int j) {
+    static void bfs(int x, int y) {
         ArrayDeque<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{i, j});
-        visited[i][j] = true;
-        distance[i][j] = 1;
+        queue.add(new int[]{x, y});
+        visited[x][y] = true;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
@@ -47,17 +39,17 @@ public class Main {
             int curY = cur[1];
 
             for (int k = 0; k < 4; k++) {
-                int nextX = curX + di[k]; // 다음 X좌표
-                int nextY = curY + dj[k]; // 다음 Y좌표
+                int nx = curX + di[k]; // 사방 탐색 시작
+                int ny = curY + dj[k];
 
-                // 미로 범위 내인가? 그렇다면 길이 있는가? 방문해본 적이 있는가?
-                if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < M && map[nextX][nextY] == 1 && !visited[nextX][nextY]) {
-                    queue.offer(new int[]{nextX, nextY}); // 큐에 다음 위치를 추가
-                    visited[nextX][nextY] = true; // 방문 처리
-                    distance[nextX][nextY] = distance[curX][curY] + 1; // 이전 위치 거리 + 1
+    if (nx >= 0 && nx < N && ny >= 0 && ny < M && visited[nx][ny] == false && map[nx][ny] == 1) {
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny});
+                    map[nx][ny] = map[curX][curY] + 1;
+                }
 
-                } // if
-            } // for
-        } // while
-    } // bfs
-} // Class
+
+            }
+        }
+    }
+}
