334430c226a418b7be53c9be351daa46b147446a
Shanate <109050481+Shanate@users.noreply.github.com>
2025-02-27 16:28:45 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.22 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split(" ");
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<arr.length; i++){
+            list.add(Integer.parseInt(arr[i]));
+        }
+        
+        Collections.sort(list);
+        
+        answer = "" + list.get(0) + " " + list.get(list.size() - 1);
+    
+        
+        return answer;
+    }
+}
