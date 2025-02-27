1b256192bb6d080e91c851db79edde7006101552
Shanate <109050481+Shanate@users.noreply.github.com>
2025-02-27 13:33:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.63 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,46 @@
 import java.util.*;
+
 class Solution {
-        static int[] dx = {-1, 0, 1, 0}; // 상 우 하 좌
-        static int[] dy = {0, 1, 0, -1};
-        static boolean[][] v;
-        
-    static int solution(int[][] maps) {
+    
+    static int[] di = {-1, 0, 1, 0}; 
+    static int[] dj = {0, -1, 0, 1};
+    static boolean[][] visited;
+    static int n;
+    static int m;
+    
+    public int solution(int[][] maps) {
         int answer = 1;
-        int n = maps.length;
-        int m = maps[0].length;
-
-        v = new boolean[n][m];
-
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[] {0, 0});
-        v[0][0] = true;
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-                int[] current = q.poll();
+        n = maps.length;
+        m = maps[0].length;
+        visited = new boolean[n][m];
+        
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0,0});
+        visited[0][0] = true;
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int num=0; num< size; num++){
+                int[] current = queue.poll();
                 int x = current[0];
                 int y = current[1];
-
-                // 도착 지점에 도달했는지 확인
-                if (x == n - 1 && y == m - 1) {
+                
+                if (x== n-1 && y == m-1){
                     return answer;
                 }
-
-                // 상하좌우 이동
-                for (int j = 0; j < 4; j++) {
-                    int newX = x + dx[j];
-                    int newY = y + dy[j];
-
-                    // 유효한 위치인지, 방문한 적이 없는지, 벽이 아닌지 확인
-                    if (newX >= 0 && newX < n && newY >= 0 && newY < m && !v[newX][newY] && maps[newX][newY] == 1) {
-                        q.add(new int[]{newX, newY});
-                        v[newX][newY] = true;
+                for(int k=0; k<4; k++){
+                    int nx = x + di[k];
+                    int ny = y + dj[k];
+                    
+                    if(nx>=0 && nx < n && ny>=0 && ny<m && !visited[nx][ny] && maps[nx][ny] == 1){
+                        queue.offer(new int[]{nx, ny});
+                        visited[nx][ny] = true;
                     }
                 }
             }
             answer++;
         }
         return -1;
+
     }
 }
