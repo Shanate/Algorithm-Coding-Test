40c00c223984cdf018cf9a687942eb070a7bbbd7
Shanate <109050481+Shanate@users.noreply.github.com>
2025-02-27 11:21:38 +0900

    [level 2] Title: 타겟 넘버, Time: 6.86 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,24 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
-        static int answer;
-
-    static void dfs(int[] numbers, int target, int depth, int sum) {
-        if(depth == numbers.length) {
-            if(target == sum) {
-                answer++;
-            }
-            return;
-        }
-        dfs(numbers, target, depth+1, sum+numbers[depth]);
-        dfs(numbers, target, depth+1, sum-numbers[depth]);
-    }
-
-    static int solution(int[] numbers, int target) {
+    static int answer;
+    public int solution(int[] numbers, int target) {
         answer = 0;
+        
         dfs(numbers, target, 0, 0);
+        
         return answer;
     }
-
+    
+    static void dfs(int[] numbers, int target, int depth, int sum){
+        if(depth == numbers.length){
+            if(target == sum){
+            answer++;
+            }
+        return;
+    }
+    dfs(numbers, target, depth+1, sum+numbers[depth]);
+    dfs(numbers, target, depth+1, sum-numbers[depth]);
+    }
 }
