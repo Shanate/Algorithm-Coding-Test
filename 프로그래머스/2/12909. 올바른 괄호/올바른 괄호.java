0fbf002c30cf895fade06ec85921e87573d72036
Shanate <109050481+Shanate@users.noreply.github.com>
2025-02-28 22:27:43 +0900

    [level 2] Title: 올바른 괄호, Time: 17.13 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,30 +3,26 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-       ArrayDeque<Character> stack = new ArrayDeque<>();
 
-        for(int i = 0; i < s.length(); i++) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
-            if(c == '(') { // 개괄호일 경우 스택에 저장
+            if(c == '('){
                 stack.push(c);
-            }
-            else if(c == ')'){ // 폐괄호일 경우
-                if(stack.isEmpty()) { // case10 스택이 비었을 때(첫장부터 밑장빼기), 탐색 종료
-                    answer = false;
-                    return answer;
-                }
-                else{   // 개괄호가 있음을 의미 -> 추출해버린다
-                    stack.pop();
-                }
+            } else if(c == ')'){
+             if(!stack.isEmpty()){
+                 stack.pop();
+             }   else {
+                 answer = false;
+                 return answer;
+             }
             }
         }
-        if(stack.isEmpty()) {
-            answer = true;
-        }
-        else {
+        if(!stack.isEmpty()){
             answer = false;
         }
 
+        
         return answer;
     }
 }
