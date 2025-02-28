54a8f30031bad768bde7e7a70c929a7952d7811c
Shanate <109050481+Shanate@users.noreply.github.com>
2025-02-28 15:11:36 +0900

    [level 2] Title: 모음 사전, Time: 23.16 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,34 +1,25 @@
-import java.io.*;
 import java.util.*;
 
-/*
-1st thinking : word의 길이는 1이상 5이하이다. => 각 자리에 들어올 수 있는 경우의 수는 5가지이다.
-5 X 5 X 5 X 5 X 5 = 3,125가 이 사전의 총 단어 개수
-A -> E가 아니라, A -> AA다.
-A -> AA -> AAA -> AAAA -> AAAAA -> AAAAE -> AAAAI -> AAAAO -> AAAAU ->
-AAAE -> AAAI -> AAAO -> AAAU -> AAE ...
-
-*/
-
 class Solution {
-        static char[] str = {'A','E','I','O','U'}; // 모음
-        static ArrayList<String> dic = new ArrayList<>();
-
-    private static void dfs(String word) {
-        if(word.length()> 5) return; // 종료조건.
-        dic.add(word);
-        for(char vowel : str) {
-                        //for(int i=0; i<str.length; i++)
-            dfs(word + vowel);
+    
+    static char[] str = {'A', 'E', 'I', 'O', 'U'};
+    static ArrayList<String> dic = new ArrayList<>();
+    
+    public int solution(String word) {
+        int answer = 0;
+        dfs("");
+        answer = dic.indexOf(word);
+        
+        return answer;
+    }
+    
+    static void dfs(String makeWord){
+        if(makeWord.length()>5) return;
+        dic.add(makeWord);
+        for(char spell : str){
+            dfs(makeWord + spell);
         }
     }
-
-    static int solution(String word){
-        int answer = 0;
-        dic.clear();
-        dfs("");
-
-
-        return dic.indexOf(word);
-    }
+    
+    
 }
