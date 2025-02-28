66b505d535ae332b1f67554ef01de58c2322f084
Shanate <109050481+Shanate@users.noreply.github.com>
2025-02-28 12:17:08 +0900

    [level 2] Title: 방문 길이, Time: 0.17 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,46 +1,50 @@
+import java.util.*;
+
 class Solution {
-    // U, D, R, L
-    static int[] di = {0, 0, 1, -1};
-    static int[] dj = {-1, 1, 0, 0};
-    static int[] opp = {1, 0, 3, 2};
+    
     static boolean[][][] visited;
+    // 상(U), 우(R), 하(D), 좌(L)
+    static int[] di = {-1, 0, 1, 0}; // 행
+    static int[] dj = {0, 1, 0, -1}; // 열
+    static int[] opp = {2, 3, 0, 1};
     
     public int solution(String dirs) {
-        int answer = 0;
+        int answer = 0; // 처음 걸어본 길의 길이 카운트
         visited = new boolean[11][11][4];
-        int curX = 5; // 현재 위치
-        int curY = 5; // 현재 위치
+        
+        int nowX = 5;
+        int nowY = 5;
         
         for(int i=0; i<dirs.length(); i++){
-            int d=0;
+            char parse = dirs.charAt(i);
+            int arrow = 0;
 
-            if(dirs.charAt(i) == 'U'){
-                d=0;
+            if(parse == 'U'){
+                arrow = 0;
             }
-            else if(dirs.charAt(i) == 'D'){
-                d=1;
+              else if(parse == 'R'){
+                arrow = 1;
+            } else if(parse == 'D'){
+                arrow = 2;
+            } else if(parse == 'L'){
+                arrow = 3;
             }
-            else if(dirs.charAt(i) == 'R'){
-                d=2;
-            }
-            else if(dirs.charAt(i) == 'L'){
-                d=3;
-            }
-            int nr = curX + di[d]; // 움직일 위치
-            int nc = curY + dj[d]; // 움직일 위치
+            int cursorX = nowX + di[arrow];
+            int cursorY = nowY + dj[arrow];
             
-            if (nr >= 0 && nr <= 10 && nc >= 0 && nc <= 10) {
-                if (!visited[curX][curY][d] && !visited[nr][nc][opp[d]]){
-                    visited[curX][curY][d] = true;
-                    visited[nr][nc][opp[d]] = true;
+            if(cursorX>=0 && cursorX<11 && cursorY>=0 && cursorY<11){
+                if(!visited[cursorX][cursorY][arrow] && !visited[nowX][nowY][opp[arrow]]){
+                    visited[cursorX][cursorY][arrow] = true;
+                    visited[nowX][nowY][opp[arrow]] = true;
                     answer++;
-            }
+                }
                 
-            curX = nr;
-            curY = nc;
+                nowX = cursorX;
+                nowY = cursorY;
+                
+            }
             
-        } // for
-    }
+        } // for i
         
         return answer;
     }
