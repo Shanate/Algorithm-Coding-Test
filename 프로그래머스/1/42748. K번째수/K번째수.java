9c6dca89fe3ccacf8a7ce64af9f8d0b3efa926e4
Shanate <109050481+Shanate@users.noreply.github.com>
2025-02-28 19:23:48 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,20 +1,18 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length]; // commands.length = 3
+        int[] answer = new int[commands.length];
         
         ArrayList<Integer> list = new ArrayList<>();
         
         for(int i=0; i<commands.length; i++){
             for(int j=commands[i][0]; j<=commands[i][1]; j++){
-                list.add(array[j-1]); // 시점 e ~ 종점 e를 list에 추가
+                list.add(array[j-1]);
             }
-            Collections.sort(list); // 배열 다 뽑아낸 후 정렬
-            answer[i] = list.get(commands[i][2]-1); // K번째 e 추출
-            list.clear(); // list 배열 초기화
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2]-1);
+            list.clear();
         }
-        
         return answer;
     }
 }
