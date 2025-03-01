e5ac93f665497fe23d00cd0700a098a882990755
Shanate <109050481+Shanate@users.noreply.github.com>
2025-03-01 09:41:47 +0900

    [level 1] Title: 폰켓몬, Time: 5.90 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,20 +1,21 @@
 import java.util.*;
-
 class Solution {
+    
     public int solution(int[] nums) {
         int answer = 0;
-        int getPokemon = nums.length / 2;
-        HashSet<Integer> set = new HashSet<>(); // 중복 원소 버리기 위해서 HashSet 사용
-
-        for(int number : nums) {
-            set.add(number); // 여기서 중복 제거
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int ponkemon : nums){
+            set.add(ponkemon);
         }
-
-        if(getPokemon >= set.size()) {
-            return set.size();
+        
+        if(nums.length/2 < set.size()){
+            return nums.length/2;
         } else {
-            return getPokemon;
+            return set.size();
         }
-//        return answer;
+    
+
     }
 }
